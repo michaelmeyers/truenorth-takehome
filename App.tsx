145477ece963865d524db2270f528d646528a577
Michael Meyers <>
2023-11-29 14:49:47 -0700

    displayed all of the starships in a FlatList and added typing as I went

--- App.tsx
+++ App.tsx
@@ -6,11 +6,9 @@
  */
 
 import React from 'react'
-import type { PropsWithChildren } from 'react'
 import {
   FlatList,
   SafeAreaView,
-  ScrollView,
   StatusBar,
   StyleSheet,
   Text,
@@ -18,13 +16,7 @@ import {
   View
 } from 'react-native'
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions
-} from 'react-native/Libraries/NewAppScreen'
+import { Colors } from 'react-native/Libraries/NewAppScreen'
 import {
   ApolloClient,
   InMemoryCache,
@@ -33,6 +25,7 @@ import {
   useQuery,
   ApolloError
 } from '@apollo/client'
+import { Float } from 'react-native/Libraries/Types/CodegenTypes'
 
 const client = new ApolloClient({
   uri: 'https://swapi-graphql.netlify.app/.netlify/functions/index',
@@ -43,6 +36,10 @@ const GET_STARSHIPS = gql`
   query GetStarships {
     allStarships {
       starships {
+        costInCredits
+        crew
+        id
+        length
         model
         name
         passengers
@@ -51,20 +48,129 @@ const GET_STARSHIPS = gql`
     }
   }
 `
-interface Starship {}
+interface Starship {
+  costInCredits: Float
+  crew: string
+  id: string
+  length: Float
+  model: string
+  name: string
+  passengers: string
+  starshipClass: string
+}
+
+interface StarshipData {
+  allStarships: {
+    starships: Starship[]
+  }
+}
 
 interface StarshipQuery {
   loading: boolean
   error?: ApolloError | undefined
-  data: Starship[] | undefined
+  data: StarshipData | undefined
+}
+
+interface StarShipItem {
+  starship: Starship
+}
+
+function StarshipItem (props: StarShipItem): JSX.Element {
+  const starship: Starship = props.starship
+  const {
+    costInCredits,
+    crew,
+    length,
+    model,
+    name,
+    passengers,
+    starshipClass
+  } = starship
+
+  const shipTableData = [
+    {
+      name: 'Model',
+      value: model
+    },
+    {
+      name: 'Class',
+      value: starshipClass
+    },
+    {
+      name: 'Crew #',
+      value: crew
+    },
+    {
+      name: 'Passenger #',
+      value: passengers
+    },
+    {
+      name: 'Ship Length',
+      value: length,
+      subValue: 'meters'
+    },
+    {
+      name: 'Cost',
+      value: costInCredits,
+      subValue: 'credits'
+    }
+  ]
+
+  return (
+    <View style={styles.itemContainer}>
+      <View style={styles.displayContainer}>
+        <View>
+          <Text style={styles.starshipName}>{name}</Text>
+        </View>
+        <View style={styles.tableView}>
+          {shipTableData.map(({ name, value, subValue }) => {
+            return (
+              <View style={styles.shipTableView}>
+                <View style={styles.shipTableLabel}>
+                  <Text style={styles.tableText}>{name}</Text>
+                </View>
+                <View style={styles.shipTableValue}>
+                  {subValue ? (
+                    <Text>
+                      <Text style={styles.tableText}>
+                        {value?.toLocaleString()}
+                      </Text>
+                      <Text style={styles.tableSubText}>{' ' + subValue}</Text>
+                    </Text>
+                  ) : (
+                    <Text style={styles.tableText}>{value}</Text>
+                  )}
+                </View>
+              </View>
+            )
+          })}
+        </View>
+      </View>
+    </View>
+  )
 }
 
 function MainView (): JSX.Element {
   const { loading, error, data }: StarshipQuery = useQuery(GET_STARSHIPS)
 
+  if (!data) {
+    return (
+      <View>
+        <Text>The data is far far away</Text>
+      </View>
+    )
+  }
+
+  const renderItem = ({ item }) => {
+    const starship: Starship = item
+    return starship && <StarshipItem starship={starship} />
+  }
+
+  const { starships } = data?.allStarships
+
   return (
-    <View style={styles.sectionContainer}>
-      <View style={{ height: 100, width: 100, backgroundColor: 'orange' }} />
+    <View style={styles.container}>
+      <FlatList data={starships} renderItem={renderItem} />
     </View>
   )
 }
@@ -78,11 +184,8 @@ function App (): JSX.Element {
 
   return (
     <ApolloProvider client={client}>
-      <SafeAreaView style={backgroundStyle}>
-        <StatusBar
-          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-          backgroundColor={backgroundStyle.backgroundColor}
-        />
+      <SafeAreaView style={{ backgroundColor: Colors.darker }}>
+        <StatusBar barStyle={'light-content'} backgroundColor={Colors.darker} />
         <MainView />
       </SafeAreaView>
     </ApolloProvider>
@@ -90,21 +193,51 @@ function App (): JSX.Element {
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24
+  container: {},
+  itemContainer: {
+    paddingVertical: 10,
+    paddingHorizontal: 10
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600'
+  displayContainer: {
+    backgroundColor: 'grey',
+    borderRadius: 5,
+    padding: 10
+  },
+  tableView: {
+    borderColor: 'black',
+    borderBottomWidth: 1
+  },
+  shipTableView: {
+    borderColor: 'black',
+    borderTopWidth: 1,
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+    flexDirection: 'row',
+    padding: 2
+  },
+  shipTableLabel: {
+    flex: 1,
+    alignItems: 'flex-start'
+  },
+  shipTableValue: {
+    flex: 2,
+    alignItems: 'flex-end'
+  },
+  tableText: {
+    fontSize: 11
+  },
+  tableSubText: {
+    fontSize: 8,
+    color: '#222222'
   },
   sectionDescription: {
     marginTop: 8,
     fontSize: 18,
     fontWeight: '400'
   },
-  highlight: {
-    fontWeight: '700'
+  starshipName: {
+    fontSize: 20,
+    fontWeight: 'bold'
   }
 })
 
